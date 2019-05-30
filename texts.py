3aea7e5333c0bcae6758e92bc8241ea77db5ab13
miramind <miramind.tech@gmail.com>
2019-05-31 01:11:44 +0700

    TTS

---
+++ texts.py
@@ -0,0 +1,10 @@
+['The N.R.A. Foundation describes itself as “America’s leading charitable organization in support of the shooting sports.',
+'Each year, the foundation—which is a nonpolitical arm of the National Rifle Association, and can accept tax-deductible contributions—takes in tens of millions of dollars, mostly from small donors.',
+"He didn’t want to sleep on the streets of a strange city, and didn’t have the money for a bus back to San Bernardino.",
+ "He decided to turn himself in. The cops picked him up and drove him to a concrete building in downtown Houston—the local office for Child Protective Services.",
+ "They had Kelvin wait in a conference room, where, a few hours later, a man in baggy khakis and thick-soled sneakers padded through the door. He looked about sixty, with ruddy cheeks, cotton-white hair, and glassy blue eyes.",
+ "His face was kind and sad. His name was James Terence Keel, but he went by Terry.",
+ "In both cases, the Mueller report’s description of ten actions by President Trump that may have constituted obstruction of justice is central to their argument.",
+"As a child, Halep was so shy that it was painful for her even to speak on the phone, but she forced herself to face the cameras and the scrutiny of the media, which grew more intense with each season."
+ "Every day, she went to the gym to tend to her muscles, joints, and ligaments. When her friends went to parties, she went to sleep."]
+
